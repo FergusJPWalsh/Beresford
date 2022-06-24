b5c651bc1f199bbcbe88a38d507d742887deff72
Fergus <36740727+FergusJPWalsh@users.noreply.github.com>
2022-06-24 23:37:20 +0100

    Part 2 vocab

--- dcc.py
+++ dcc.py
@@ -1,7 +1,5 @@
 import csv
 import re
-from progress.bar import Bar
-
 
 dcc_headwords_in = []
 
@@ -17,6 +15,7 @@ for item in dcc_headwords_in:
     part = item.partition(" ")[0]
     dcc_headwords.append(part)
 
+dcc_headwords = sorted(dcc_headwords)
 
 beresford = []
 
@@ -25,26 +24,24 @@ with open("vocab_beresford_2.tsv", "r", encoding="utf-8") as g:
     for row in reader:
         beresford.append(row)
 
-dcc_i = []
+unique = []
+vocab_set = set()
 
-with Bar("Working...", max=len(beresford)) as bar:
+for i, row in enumerate(beresford):
+    token = row[1]
+    if token not in vocab_set:
+        unique.append(row)
+        vocab_set.add(token)
 
-    for i, row in enumerate(beresford):
-        row_1 = row[1]
-        for headword in dcc_headwords:
-            if re.match(f"^{headword}$", row_1):
-                dcc_i.append(i)
-                # print(headword,"->",row_1,"\n",beresford[i])
-                bar.next()
+not_dcc = []
+for i, row in enumerate(unique):
+    token = row[1]
+    if token not in dcc_headwords:
+        not_dcc.append(row)
 
-print(len(beresford))
-print(len(dcc_i))
-for i in sorted(dcc_i, reverse=True):
-    del beresford[i]
+print("original list",len(beresford))
+print("list with unique lemmata and with DCC removed",len(not_dcc))
 
-
-
-print(len(beresford))
 with open("vocab_beresford_2_no_dcc.tsv", "w", newline="", encoding="utf-8") as o:
     writer = csv.writer(o, delimiter="\t")
-    writer.writerows(beresford)
+    writer.writerows(not_dcc)
